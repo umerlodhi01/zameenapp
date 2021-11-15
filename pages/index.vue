3d70987ae292ae678b16476a2c7652094c4caa21
Umer Lodhi <umerlodhi619@gmail.com>
2021-11-15 19:25:58 +0500

    route
    route

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,13 @@
-<template>
-
-</template>
+<template></template>
 
 <script>
-
-  export default {
-    middleware: ['auth'],
-    created() {
-      let subDomain = this.$cookies.get('subdomain');
-        if (subDomain === 'admin' || subDomain === 'vm-admin')
-            this.$router.push('/admin/auth/login');
-        else
-            this.$router.push('/' + subDomain + '/auth/login');
-    }
-  }
+export default {
+  middleware: ["auth"],
+  created() {
+    this.$router.push("/admin/auth/login");
+  },
+};
 </script>
 
 <style>
-
 </style>
