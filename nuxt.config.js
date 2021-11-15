18c09114fa66919e035ca4132d143743759bb711
Umer Lodhi <umerlodhi619@gmail.com>
2021-11-15 19:17:02 +0500

    host
    host

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,9 @@ import colors from 'vuetify/es5/util/colors'
 export default {
     // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
     ssr: false,
-
+    server: {
+        host: '0.0.0.0' // default: localhost
+    },
     // Global page headers: https://go.nuxtjs.dev/config-head
     head: {
         titleTemplate: '%s - zameenapp',
