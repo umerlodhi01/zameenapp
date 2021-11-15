6bb114eec01ccbd3f45cc4aa6f1cd2c10fca60b6
Umer Lodhi <umerlodhi619@gmail.com>
2021-11-15 19:04:56 +0500

    name changed
    name changed

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     apps: [{
-        name: 'NuxtAppName',
+        name: 'zameenapp',
         exec_mode: 'cluster',
         instances: 'max', // Or a number of instances
         script: './node_modules/nuxt/bin/nuxt.js',
